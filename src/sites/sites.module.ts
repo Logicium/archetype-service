f55c488b7a2112c7a7b6fc48622ef4fd43773c13
Kisora Thomas <kisora.thomas@gmail.com>
2026-05-18 21:36:24 -0600

    Integrate BullMQ queue for site update processing in Sites module

--- src/sites/sites.module.ts
+++ src/sites/sites.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
 import { MikroOrmModule } from '@mikro-orm/nestjs'
+import { BullModule } from '@nestjs/bullmq'
 import { Site } from '../entities/site.entity'
 import { SiteContent } from '../entities/site-content.entity'
 import { DeployLog } from '../entities/misc.entity'
@@ -7,9 +8,15 @@ import { SitesService } from './sites.service'
 import { AdminSitesController, PublicSitesController } from './sites.controller'
 import { AuthModule } from '../auth/auth.module'
 import { ProvisioningModule } from '../provisioning/provisioning.module'
+import { SITE_UPDATE_QUEUE } from '../provisioning/provisioning.constants'
 
 @Module({
-  imports: [MikroOrmModule.forFeature([Site, SiteContent, DeployLog]), AuthModule, ProvisioningModule],
+  imports: [
+    MikroOrmModule.forFeature([Site, SiteContent, DeployLog]),
+    BullModule.registerQueue({ name: SITE_UPDATE_QUEUE }),
+    AuthModule,
+    ProvisioningModule,
+  ],
   controllers: [PublicSitesController, AdminSitesController],
   providers: [SitesService],
   exports: [SitesService],
